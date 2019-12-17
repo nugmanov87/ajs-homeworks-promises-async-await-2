7c5eaffedf54c6340a5b351729c61a1488b3687b
Ruslan <ruslan-nugmanov87@mail.ru>
2019-12-17 21:36:34 +0500

    2 задание 10 лекция

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/8m7kfjg7xjlejx00?svg=true)](https://ci.appveyor.com/project/nugmanov87/ajs-homeworks-promises-async-await-2)
---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable class-methods-use-this */
+import readGameSaving from './readGameSaving';
+
+class GameSavingData {
+  constructor(data) {
+    this.data = data;
+  }
+
+  json() {
+    return new Promise((resolve) => {
+      // эмуляция обработки ArrayBuffer
+      setTimeout(() => {
+        resolve(String.fromCharCode.apply(null, new Uint16Array(this.data)));
+      }, 1000);
+    });
+  }
+}
+
+export default class GameSavingLoader {
+  load() {
+    return async function gameSaving() {
+      try {
+        const data = await readGameSaving();
+        const gameSavingData = new GameSavingData(data);
+        const str = await gameSavingData.json();
+        const result = await JSON.parse(str);
+        return result;
+      } catch (e) {
+        return 'Ошибка';
+      }
+    };
+  }
+}
